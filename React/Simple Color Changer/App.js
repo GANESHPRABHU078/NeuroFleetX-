1588bc3c08d1714087231ee282933cda36d6f89c
GANESHPRABHU078 <gp561910@gmail.com>
2025-10-15 19:54:08 +0530

    Create App.js

---
+++ React/Simple Color Changer/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("white");
+
+  const changeColor = () => {
+    const colors = ["#ffadad", "#caffbf", "#9bf6ff", "#ffd6a5", "#bdb2ff"];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    setColor(randomColor);
+  };
+
+  return (
+    <div
+      style={{
+        backgroundColor: color,
+        height: "100vh",
+        textAlign: "center",
+        paddingTop: "100px",
+        transition: "0.5s",
+      }}
+    >
+      <h1>🎨 Color Changer App</h1>
+      <button onClick={changeColor}>Change Color</button>
+    </div>
+  );
+}
+
+export default App;
