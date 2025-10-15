fc965f0d59f94a4425b063e7106a94ae27792f5b
GANESHPRABHU078 <gp561910@gmail.com>
2025-10-15 19:50:19 +0530

    Create App.js

---
+++ React/Counter App/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "80px" }}>
+      <h1>🧮 Counter App</h1>
+      <h2>{count}</h2>
+      <button onClick={() => setCount(count + 1)}>➕ Increment</button>
+      <button onClick={() => setCount(count - 1)}>➖ Decrement</button>
+    </div>
+  );
+}
+
+export default App;
