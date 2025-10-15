cbc814db43e30707efe3a3ebc1e98f8dc59dcf90
GANESHPRABHU078 <gp561910@gmail.com>
2025-10-15 19:38:45 +0530

    Create App.js

---
+++ React/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+function App() {
+  const name = "Ganesh";
+  return (
+    <div className="App">
+      <h1>👋 Welcome to React</h1>
+      <p>Hello, {name}! This is your first React app.</p>
+    </div>
+  );
+}
+
+export default App;
